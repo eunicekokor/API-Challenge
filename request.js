0f427b518c19a15da55010966342ed263c16709f
eunicekokor@gmail.com <eunicekokor@gmail.com>
2014-12-01 15:40:54 -0500

    received access code!

---
+++ request.js
@@ -0,0 +1,9 @@
+var request = '{"email":"eek2138@columbia.edu", "github": "https://github.com/eunicekokor/API-Challenge"}';
+var url = "http://challenge.code2040.org/api/register";
+var json = JSON.parse(request);
+
+var xmlHttp = null;
+xmlHttp = new XMLHttpRequest();
+xmlHttp.open( "POST", url, false );
+xmlHttp.send(request);
+console.log(xmlHttp.responseText);
