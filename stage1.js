b890fa25a0cb2f085918050bc0e610aabf28120b
eunicekokor@gmail.com <eunicekokor@gmail.com>
2014-12-01 23:04:29 -0500

    finished first stage!

--- stage1.js
+++ stage1.js
@@ -1,18 +1,31 @@
+//initial urls & json strings
 var url = "http://challenge.code2040.org/api/getstring";
-var request1 = '{"token":"cn6dm0cSvX"}';
+var url2 = "http://challenge.code2040.org/api/validatestring";
+var request1 = '{"token":"cn6dm0cSvX", "string" : ""}';
 
+//access to server by putting in my token
 var xmlHttp = null;
 xmlHttp = new XMLHttpRequest();
 xmlHttp.open("POST", url, false);
-xmlHttp.send(request1);
-var test = xmlHttp.responseText;
-var string = revString(test);
-var request2 = '{"token": string}';
+xmlHttp.send(request1); //submit token;
 
+//get data respose from server & reverse string
+var test = xmlHttp.responseText;
+var final = JSON.parse(test);
+console.log(final);
+console.log(final['result']); //actual string from the server
+var string = revString(final['result']); //reversed string from server
+
+//JSON manipulation (took the longest time to understand differences!)
+request1 = JSON.parse(request1); //JSON string to JSON object
+request1['string'] = string; //changed JSON key value
+console.log(request1); //checking JSON Object contents
+
+//POST JSON String to server
 var xml2 = new XMLHttpRequest();
-xml2.open("POST", url, false);
-xml2.send(request2);
-console.log(xml2.responseText);
+xml2.open("POST", url2, false);
+xml2.send(JSON.stringify(request1));
+console.log(JSON.parse(xml2.responseText));
 
 function revString(input) {
   var reversed = "";
@@ -20,5 +33,3 @@ function revString(input) {
     reversed += input[i];
   return reversed;
 }
-
-
